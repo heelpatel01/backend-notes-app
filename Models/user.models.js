4896f2c00f461c2a257e7d39bbdad9065acce590
HEEL PATEL <heelpatel216@gmail.com>
2024-08-04 19:37:13 +0530

    Yo Back

--- Models/user.models.js
+++ Models/user.models.js
@@ -1,30 +1,28 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
-  fullName: {
-    type: String,
-    required: [true, "Full name is required"],
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: [true, "Email is required"],
-    unique: true,
-    trim: true,
-    lowercase: true,
-  },
-  password: {
-    type: String,
-    required: [true, "Password is required"],
-    minlength: 6,
-  },
-  createdOn: {
-    type: Date,
-    default: Date.now,
-  },
+ fullName: {
+  type: String,
+  required: [true, "Full name is required"],
+  trim: true,
+ },
+ email: {
+  type: String,
+  required: [true, "Email is required"],
+  unique: true,
+  trim: true,
+  lowercase: true,
+ },
+ password: {
+  type: String,
+  required: [true, "Password is required"],
+  minlength: 6,
+ },
+ createdOn: {
+  type: Date,
+  default: Date.now,
+ },
 });
-
 
 const User = mongoose.model("User", userSchema);
 
